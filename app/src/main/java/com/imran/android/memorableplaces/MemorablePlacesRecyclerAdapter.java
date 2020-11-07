be50030d990c336fa50348d98b2bc0460223fb63
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-07 13:09:33 +0600

    recycler adapter

--- app/src/main/java/com/imran/android/memorableplaces/MemorablePlacesRecyclerAdapter.java
+++ app/src/main/java/com/imran/android/memorableplaces/MemorablePlacesRecyclerAdapter.java
@@ -1,17 +1,29 @@
 package com.imran.android.memorableplaces;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 /**
  * Created by Shah Md Imran Hossain on 07, November, 2020
  */
 public class MemorablePlacesRecyclerAdapter extends RecyclerView.Adapter<MemorablePlacesRecyclerAdapter.ViewHolder> {
+    private final Context context;
+    private final LayoutInflater layoutInflater;
+    private final List<String> memorablePlaces;
 
-
+    public MemorablePlacesRecyclerAdapter(Context context, List<String> memorablePlaces) {
+        this.context = context;
+        layoutInflater = LayoutInflater.from(context);
+        this.memorablePlaces = memorablePlaces;
+    }
+    
     @NonNull
     @Override
     public MemorablePlacesRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
