a6e66efa6364deb660f9cacb70be20a63999ae4d
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-08 14:52:41 +0600

    maps activity

--- app/src/main/java/com/imran/android/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/imran/android/memorableplaces/MapsActivity.java
@@ -31,6 +31,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
