695a8a3b4e6b3bbf2a4469708d946279744e68f8
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-08 12:16:57 +0600

    recycler adapter

--- app/src/main/java/com/imran/android/memorableplaces/MainActivity.java
+++ app/src/main/java/com/imran/android/memorableplaces/MainActivity.java
@@ -1,14 +1,28 @@
 package com.imran.android.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    List<String> placesName = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        placesName.add("Add a new place...");
+
+        final RecyclerView recyclerView = findViewById(R.id.placeRecyclerView);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        
     }
 }
